182c58abd125925da580c1d51d25bf1daf13fdc8
Matthew Moltzau <matthew@moltzau.net>
2017-05-03 12:06:20 -0600

    collisions and such

---
+++ xSquadron/Assets/IncomingShips.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncomingShips : MonoBehaviour {
+	public GameObject warningText;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag ("Player")) {
+			
+		}
+	}
+}
